ba427d2e41bc6f5f0269f3bea13d22f4296202dd
danilomillan <raul20ulra@gmail.com>
2023-06-02 15:30:06 -0500

    Syntax

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get ,Param} from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+    
+    @Get()
+    getAllCars() {
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ) {
+        return this.cars[id]
+    }
+}
